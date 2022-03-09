4066f94549fd1e4fc48aab81a333fc7acdb51d94
Hrottek <xhrottek@stuba.sk>
2022-03-09 18:21:34 +0100

    Final Commit

--- src/main/java/sk/stuba/fei/uim/OOP/App.java
+++ src/main/java/sk/stuba/fei/uim/OOP/App.java
@@ -1,4 +1,22 @@
 package sk.stuba.fei.uim.OOP;
 
+import javax.swing.*;
+
 public class App {
+    public static void main(String[] args) {
+
+        int a =  (int)(Math.random() * 999);
+        int b = (int)(Math.random() * 999);
+
+        if(a < b)
+            System.out.println("0");
+
+        else
+            System.out.println("1");
+        System.out.print("Hodnota A je:");
+        System.out.println(a);
+        System.out.print("Hodnota B je:");
+        System.out.print(b);
+    }
+
 }
